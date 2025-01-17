672ab759ef6dbc812a34b45ec84e9912037abdcd
Daffa Arditya <ardtys19@gmail.com>
2025-01-17 11:38:12 +0700

    New file

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   output: 'server',
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  adapter: netlify()
 });
